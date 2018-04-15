816b5e6bd77f26d5f1d58ac464336d4fe5254935
Ruiqi <ruiqi.zhong@gatech.edu>
2018-04-15 16:43:56 -0400

    implemented are_team, act_together, surrogate_act_together, adv, in_between

--- Features_rz.py
+++ Features_rz.py
@@ -1,5 +1,57 @@
 from Corpus import *
+import nltk
+from nltk.corpus import stopwords
 
+stopwords_set = set(stopwords.words('english'))
+
+'''
+    Util functions
+'''
+# return a list of tokenids of words that a character is the deprel ('nsubj' or 'dobj')
+def char_deprel_set(sentence, char_id, deprel, debug=False):
+    token_id_set = set()
+    
+    # the dataframe containing all the tokens (in the sentence) and their info
+    df = sentence.df
+
+    # code logics ...
+    # retrieving the data in the form of lists that is convenient to deal with
+    correctedCharIds = df['correctedCharId'].values
+    deprels = df['deprel'].values
+    tokenIds = df['tokenId'].values
+    headTokenIds = df['headTokenId'].values
+
+    for token_idx in range(sentence.num_tokens):
+        headTokenId = headTokenIds[token_idx]
+        if headTokenId == -1:
+            continue
+        if correctedCharIds[token_idx] == char_id:
+            if deprels[token_idx] == deprel:
+                token_id_set.add(headTokenId)
+            if deprels[token_idx] == 'conj':
+                if df.ix[headTokenId]['deprel'] == deprel and df.ix[headTokenId]['headTokenId'] != -1:
+                    token_id_set.add(df.ix[headTokenId]['headTokenId'])
+    if debug:
+        print('extracted words are: '
+              + ' '.join([df.ix[token_idx]['originalWord'] for token_idx in token_id_set]))
+    return token_id_set
+
+# given a sentence and char pair, return a list of tokenIds for which char1/2 is nsubj/dobj
+def get_general_deprel_sets(sentence, char_pair):
+    character_id1, character_id2 = char_pair
+    # according to paper, sharing the same verbs as subj/dobj, but does not seem to work
+    char1_sub_set = char_deprel_set(sentence, character_id1, 'nsubj')
+    char2_sub_set = char_deprel_set(sentence, character_id2, 'nsubj')
+    char1_obj_set = char_deprel_set(sentence, character_id1, 'dobj')
+    char2_obj_set = char_deprel_set(sentence, character_id2, 'dobj')
+    return char1_sub_set, char2_sub_set, char1_obj_set, char2_obj_set
+
+def get_original_word(sentence, token_idx_set):
+    return [sentence.df.ix[tokenId]['originalWord'] for tokenId in token_idx_set]
+
+'''
+    Core Implementations
+'''
 # implement the “are team” feature
 # returns a boolean
 def are_team(sentence, char_pair):
@@ -21,21 +73,11 @@ def are_team(sentence, char_pair):
     headTokenIds = df['headTokenId'].values
     
     # according to paper, sharing the same verbs as subj/dobj, but does not seem to work
-    char1_sub_set, char2_sub_set, char1_obj_set, char2_obj_set = set(), set(), set(), set()
+    char1_sub_set, char2_sub_set, char1_obj_set, char2_obj_set = get_general_deprel_sets(sentence, char_pair)
     
     for token_idx in range(sentence.num_tokens):
         if correctedCharIds[token_idx] not in [character_id1, character_id2]:
             continue
-        if correctedCharIds[token_idx] == character_id1:
-            if deprels[token_idx] == 'nsubj':
-                char1_sub_set.add(headTokenIds[token_idx])
-            elif deprels[token_idx] == 'dobj':
-                char1_obj_set.add(headTokenIds[token_idx])
-        elif correctedCharIds[token_idx] == character_id2:
-            if deprels[token_idx] == 'nsubj':
-                char2_sub_set.add(headTokenIds[token_idx])
-            elif deprels[token_idx] == 'dobj':
-                char2_obj_set.add(headTokenIds[token_idx])
     
         # using conj dependency relation
         headTokenId = headTokenIds[token_idx]
@@ -45,20 +87,108 @@ def are_team(sentence, char_pair):
 
     return len(char1_sub_set & char2_sub_set) != 0 or len(char1_obj_set & char2_obj_set) != 0
 
+def _act_together(sentence, char_pair):
+    char1_sub_set, char2_sub_set, char1_obj_set, char2_obj_set = get_general_deprel_sets(sentence, char_pair)
+    token_idx_set = (char1_sub_set & char2_obj_set) | (char2_sub_set & char1_obj_set)
+    return token_idx_set
+
+def _surrogate_act_together(sentence, char_pair):
+    correctedCharIds = sentence.df['correctedCharId'].values
+    for c in correctedCharIds:
+        if c != -1 and c not in char_pair:
+            return set()
+    char1_sub_set, char2_sub_set, char1_obj_set, char2_obj_set = get_general_deprel_sets(sentence, char_pair)
+    token_idx_set = char1_sub_set | char2_sub_set | char1_obj_set | char2_obj_set \
+        - ((char1_sub_set & char2_obj_set) | (char2_sub_set & char1_obj_set)) \
+        - ((char1_sub_set & char1_obj_set) | (char2_sub_set & char2_obj_set))
+    return token_idx_set
+
+def _adverbs_used(sentence, char_pair):
+    token_idx_set = set()
+    verbs_idx = _act_together(sentence, char_pair) | _surrogate_act_together(sentence, char_pair)
+    df = sentence.df
+    deprels = df['deprel'].values
+    tokenIds = df['tokenId'].values
+    headTokenIds = df['headTokenId'].values
+    for token_idx in range(sentence.num_tokens):
+        headTokenId = headTokenIds[token_idx]
+        if headTokenId in verbs_idx and deprels[token_idx] == 'advmod':
+            token_idx_set.add(tokenIds[token_idx])
+    return token_idx_set
+
+def _in_between(sentence, char_pair):
+    # getting the char ids of interest
+    character_id1, character_id2 = char_pair
+    
+    # the dataframe containing all the tokens (in the sentence) and their info
+    df = sentence.df
+    
+    # the raw text of the original sentence
+    sentence_form = sentence.sentence_form
+    
+    # code logics ...
+    # retrieving the data in the form of lists that is convenient to deal with
+    correctedCharIds = df['correctedCharId'].values
+    deprels = df['deprel'].values
+    tokenIds = df['tokenId'].values
+    headTokenIds = df['headTokenId'].values
+
+    start, start_charId = 0, None
+    for token_idx in range(sentence.num_tokens):
+        if correctedCharIds[token_idx] in char_pair:
+            start, start_charId = token_idx, correctedCharIds[token_idx]
+            break
+    if start_charId is None:
+        return set()
+    for token_idx in reversed(range(sentence.num_tokens)):
+        if correctedCharIds[token_idx] in char_pair and correctedCharIds[token_idx] != start_charId:
+            end, end_charId = token_idx, correctedCharIds[token_idx]
+            break
+    if end_charId is None:
+        return set()
+    token_idx_set = set([tokenIds[token_idx] for token_idx in range(start + 1, end)])
+    return token_idx_set
+
+'''
+    Wrappers
+'''
+def act_together(sentence, char_pair):
+    return get_original_word(sentence, _act_together(sentence, char_pair))
+
+def surrogate_act_together(sentence, char_pair):
+    return get_original_word(sentence, _surrogate_act_together(sentence, char_pair))
+
+def adverbs_used(sentence, char_pair):
+    return get_original_word(sentence, _adverbs_used(sentence, char_pair))
+
+def in_between(sentence, char_pair):
+    wordlist = get_original_word(sentence, _in_between(sentence, char_pair))
+    print(wordlist)
+    return [w for w in wordlist if w not in stopwords_set]
+
 if __name__ == '__main__':
     # reading the texts and picking a sentence
-    author_name = 'albee.dream' # file name
+    # author_name = 'albee.dream' # file name
+    author_name = 'agee.death' # file names
     passage = Passage(author_name)
     sequence_dict = passage.sequence_dict
     # char_pair = [char_pair for char_pair in sequence_dict][0] # which character pair to pick
-    char_pair = (1,3)
+    char_pair = (0,1)
     sentence_array = sequence_dict[char_pair].sentence_seq # sentence array is
-    sentence = sentence_array[6] # getting the first sentence of the sequence
+    sentence = sentence_array[1] # getting the first sentence of the sequence
     print('The sentence of interest')
     print(sentence)
     print('The character pair of interest')
     print(char_pair)
-    act_together_feature = are_team(sentence, char_pair)
-    print(act_together_feature)
-
+    print(char_deprel_set(sentence, 2, 'dobj', debug=True))
+    are_team_feature = are_team(sentence, char_pair)
+    print(are_team_feature)
+    act_together_words = act_together(sentence, char_pair)
+    print(act_together_words)
+    surrogate_act_together_words = surrogate_act_together(sentence, char_pair)
+    print(surrogate_act_together_words)
+    adverb_used_words = adverbs_used(sentence, char_pair)
+    print(adverb_used_words)
+    in_between_words = in_between(sentence, char_pair)
+    print(in_between_words)
 
