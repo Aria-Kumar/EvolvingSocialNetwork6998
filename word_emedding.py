aa8bec28cf8c4b0bb3fb9c433f90ece9f0894484
Alex Calderwood <alexander.d.calderwood@gmail.com>
2018-04-19 00:52:49 -0400

    Added ability to get vectors from word embedding. Added functionality to make wordvectors directly from sparknotes summaries. Untested.

--- word_emedding.py
+++ word_emedding.py
@@ -1,17 +1,25 @@
 import gensim
 import logging
 import os
+import sys
+
 
 class WordEmbedding():
 
-    def __init__(self, load=False, training_sentences=None, save=True):
+    def __init__(self, load_from=None, training_sentences_file=None, save_to=None):
+        """
+        :param load_from: If not None, load a pretrained model from this path
+        :param training_sentences_file: If none, use the sentences provided. Otherwise, load the sentences from this path.
+        :param save: If not None, save a model to this path
+        """
+
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
         self.data_path = 'Data_EvolvingRelationships_Chaturvedi_AAAI2017/originalText/'
-        self.model_path = 'models/sparknotes.w2v'
 
-        if load:
-            self.model = gensim.models.Word2Vec.load(self.model_path)
+
+        if load_from:
+            self.model = gensim.models.Word2Vec.load(load_from)
 
         else:
             self.threads = 10
@@ -19,10 +27,16 @@ class WordEmbedding():
             # Size of the embedded vectors
             self.embedding_size = 300
 
-            if training_sentences is None:
+            if training_sentences_file is None:
                 print('reading sentences from', self.data_path)
                 self.sentences = self.read_default_sentences()
                 print('read {} lines'.format(len(self.sentences)))
+
+            else:
+                print('reading sentences from', self.data_path)
+                self.sentences = self.read_sentences_from_corpus(training_sentences_file)
+                print('read {} lines'.format(len(self.sentences)))
+
 
             # Create bigrams for common phrases in the model
             bigram_transformer = gensim.models.Phrases(self.sentences)
@@ -34,9 +48,9 @@ class WordEmbedding():
 
             print('trained', self.model)
 
-        if save:
-            print('saving to', self.model_path)
-            self.model.save(self.model_path)
+        if save_to:
+            print('saving to', save_to)
+            self.model.save(save_to)
 
     def read_default_sentences(self):
 
@@ -52,7 +66,30 @@ class WordEmbedding():
 
         return sentences
 
+    def read_sentences_from_corpus(self, sentences_file_path):
+
+        try:
+            file = open(sentences_file_path, 'r')
+        except IOError:
+            print('Error. Could not load file at {}. Aborting'.format(sentences_file_path))
+            sys.exit(1)
+
+        sentences = []
+
+        for line in file.readlines():
+            line = line.strip().lower().split(' ')
+            if line != '':
+                sentences.append(line)
+
+        return sentences
+
+    def vector(self, word):
+        """Return the vector for the given word"""
+        return self.model.wv[word]
+
+
 
 if __name__ == '__main__':
 
-    WordEmbedding()
+    # Create a word embedding from the downloaded sparknotes corpus
+    WordEmbedding(save_to='models/sparknotes.w2v', training_sentences_file='corpus_data/sparknotes.txt')
